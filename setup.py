196753491e2d7e7f6ba58f400f21a6f74462efd4
Tanya Dayal <your_email@example.com>
2025-12-29 19:43:08 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Medical_Chatbot',
+    version='0.1.0',
+    author='Tanya Dayal',
+    author_email='dayalt232@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
